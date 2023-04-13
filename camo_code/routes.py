c383cfce783400e05bc75014cb7d57d8eb7bf3fa
CHRISHA1122 <chrisha1122@outlook.com>
2023-04-13 13:42:10 +0000

    Added rofile and register .html

--- camo_code/routes.py
+++ camo_code/routes.py
@@ -3,8 +3,62 @@ from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from camo_code import app, db
 from camo_code.models import User, Post, Comment
+from camo_code.forms import LoginForm, RegistrationForm, UpdateProfileForm
 
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid email or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(request.args.get("next") or url_for("home"))
+    return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now a registered user!")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>", methods=["GET", "POST"])
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = UpdateProfileForm()
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your profile has been updated.")
+        return redirect(url_for("user", username=user.username))
+    elif request.method == "GET":
+        form.username.data = user.username
+        form.about_me.data = user.about_me
+    return render_template("user.html", user=user, form=form)
